7d6c0c16880318fbc1b13281de866cbc04b28895
kundan.bhandarkar@westpac.com.au <kundan.bhandarkar@westpac.com.au>
2023-04-06 10:59:05 +0530

    first commit message KB

---
+++ manageemployees/src/main/java/com/example/training/manageemployees/ManageemployeesApplication.java
@@ -0,0 +1,13 @@
+package com.example.training.manageemployees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageemployeesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageemployeesApplication.class, args);
+	}
+
+}
---
+++ manageemployees/src/test/java/com/example/training/manageemployees/ManageemployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.training.manageemployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageemployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
